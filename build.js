adf24cb91a1f9a43e51cadda93539145dc30fdc6
Jesús Ferretti <jferrettiboke@gmail.com>
2016-03-20 20:16:15 +0100

    Add recursive mode to read Markdown files

--- build.js
+++ build.js
@@ -1,25 +1,33 @@
 var fs = require('fs');
 var path = require('path');
 var fm = require('front-matter');
+var recursive = require('recursive-readdir');
 
-var collection = [];
 var dir = path.join(__dirname, 'data');
+var collection = [];
 
-fs.readdirSync(dir).forEach(function(file) {
-  var fileName = file.split('.');
-  var fileIndex = fileName.length - 1;
-  var fileExtension = fileName[fileIndex];
-  if (fileExtension !== 'md') return;
-  var data = fs.readFileSync(dir + '/' + file, 'utf8');
-  var content = fm(data);
-  collection.push({
-    title: content.attributes.title,
-    slug: content.attributes.slug,
-    date: content.attributes.date,
-    author: content.attributes.author,
-    layout: content.attributes.layout,
-    content: content.body,
+recursive(dir, function (err, files) {
+  files.forEach(function(file) {
+    var fileName = file.split('data');
+    var fileExtension = file.split('.');
+
+    fileName = fileName[fileName.length - 1];
+    fileExtension = fileExtension[fileExtension.length - 1];
+
+    if (fileExtension !== 'md') return;
+
+    var data = fs.readFileSync(path.join(__dirname, 'data', fileName), 'utf8');
+    var content = fm(data);
+
+    collection.push({
+      title: content.attributes.title,
+      slug: content.attributes.slug,
+      date: content.attributes.date,
+      author: content.attributes.author,
+      layout: content.attributes.layout,
+      content: content.body,
+    });
   });
-});
 
-fs.writeFileSync('./data/__collection.json', JSON.stringify(collection, null, '\t'), 'utf-8');
+  fs.writeFileSync('./data/__collection.json', JSON.stringify(collection, null, '\t'), 'utf-8');
+});
