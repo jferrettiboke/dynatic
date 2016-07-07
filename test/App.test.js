a8074993d8205826b80a9b9101232438dc4fc33b
Jesús Ferretti <jferrettiboke@gmail.com>
2016-07-07 18:04:56 +0200

    Remove app test

--- test/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow, mount, render } from 'enzyme';
-
-describe('<aaasasasasasas />', () => {
-  it('should render `Apsssssssssssssssssssp`', () => {
-    expect('sssssssssss').to.equal('zzzzzzzzzzzzzzz');
-  });
-});
